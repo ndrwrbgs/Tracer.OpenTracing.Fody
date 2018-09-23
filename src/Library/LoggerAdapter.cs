30dfeff8b03ef59ba7ce02eb64539a263b0c2f7f
ndrwrbgs <ndrwrbgs@users.noreply.github.com>
2018-09-23 14:54:15 -0700

    Initial

---
+++ src/Library/LoggerAdapter.cs
@@ -0,0 +1,48 @@
+﻿namespace Tracer.OpenTracing
+{
+    using System;
+    using System.Diagnostics;
+    using global::OpenTracing.Util;
+    using JetBrains.Annotations;
+
+    [PublicAPI]
+    public class LoggerAdapter
+    {
+        private readonly string name;
+
+        public LoggerAdapter(Type containingType)
+        {
+            this.name = containingType != null ? containingType.Name + "." : null;
+        }
+
+        public void TraceEnter(
+            string methodInfo,
+            string[] paramNames,
+            object[] paramValues)
+        {
+            if (!GlobalTracer.IsRegistered())
+            {
+                Trace.TraceError($"Attempted to trace a {nameof(this.TraceEnter)} operation with {typeof(LoggerAdapter).FullName} before {nameof(GlobalTracer.Register)} was called.");
+                return;
+            }
+
+            GlobalTracer.Instance
+                .BuildSpan($"{this.name}{methodInfo}")
+                .StartActive();
+        }
+
+        public void TraceLeave(
+            string methodInfo,
+            long startTicks,
+            long endTicks,
+            string[] paramNames,
+            object[] paramValues)
+        {
+            // TODO: This is dangerous, but only if folks misused tracing anyway. It will obscure their bugs, but not cause its own
+            GlobalTracer.Instance
+                .ScopeManager
+                .Active
+                .Dispose();
+        }
+    }
+}
---
+++ src/Library/RequiredByTracer/TracerAttributes.cs
@@ -0,0 +1,37 @@
+﻿// ReSharper disable once CheckNamespace - Required by Tracer.Fody
+namespace TracerAttributes
+{
+    using System;
+
+    /// <summary>
+    /// Namespace and type are required for projects of Tracer.Fody.
+    /// TODO: Suggest an .Interfaces style import for Tracer.Fody similar to ArgValidation.Fody
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
+    public class TraceOn : Attribute
+    {
+        public TraceTarget Target { get; set; }
+
+        public TraceOn()
+        {
+        }
+
+        public TraceOn(TraceTarget traceTarget)
+        {
+            Target = traceTarget;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
+    public class NoTrace : Attribute
+    {
+    }
+
+    public enum TraceTarget
+    {
+        Public,
+        Internal,
+        Protected,
+        Private
+    }
+}
